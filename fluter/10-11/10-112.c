f06bb083b6c1d388de72888793097aaa43832578
poojagithub29 <148519448+poojagithub29@users.noreply.github.com>
2023-11-26 22:23:44 +0530

    Add files via upload

---
+++ fluter/10-11/10-112.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2;
+    printf("enter first number:");
+    scanf("%d", n1);
+    printf("enter second number:");
+    scanf("%d", n2);
+    int a[0];
+
+    for (int i = n1; i <= n2; i++)
+    {
+
+        {
+            printf("%d\n", a[i]);
+        }
+    }
+
+    return 0;
+}
